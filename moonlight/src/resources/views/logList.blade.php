f48266197a89fac3b7b5edd26587782e97770764
Denis Shumeev <denis-shumeev@yandex.ru>
2018-01-19 02:58:12 +0300

    Log.

--- moonlight/src/resources/views/logList.blade.php
+++ moonlight/src/resources/views/logList.blade.php
@@ -8,14 +8,19 @@
         <ul class="log">
         @foreach ($userActions as $userAction)
         <li>
-            @if ($userAction->user->photoExists())
+            @if ($userAction->user && $userAction->user->photoExists())
             <div class="avatar"><img src="{{ $userAction->user->getPhotoSrc() }}" /></div>
             @else
             <div class="avatar"><img src="/packages/moonlight/img/avatar.png" /></div>
             @endif
             <div class="date">{{ $userAction->created_at->format('d.m.Y') }}<br><span class="time">{{ $userAction->created_at->format('H:i:s') }}</span></div>
+            @if ($userAction->user)
             <span class="user">{{ $userAction->user->login }}</span>
             <small>{{ $userAction->user->first_name }} {{ $userAction->user->last_name }}</small><br>
+            @else
+            <span class="user">User.{{ $userAction->user_id }}</span>
+            <small>Удаленный пользователь</small><br>
+            @endif
             <span class="title">{{ $userAction->getActionTypeName() }}</span>
             <span>{{ $userAction->comments }}</span>
         </li>
