d55a95eb926ce1a0ed960c00faf5c2c0f97e3369
Denis Shumeev <denis-shumeev@yandex.ru>
2018-02-14 12:47:26 +0300

    CSS.

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -11,19 +11,18 @@
 
 @foreach ($questions as $k => $question)
     @if (isset($questionAnswered[$question->id]))
-    <div class="row complete {{ $questionAnswered[$question->id]->correct ? 'correct' : 'incorrect' }}">
+    <div class="question complete {{ $questionAnswered[$question->id]->correct ? 'correct' : 'incorrect' }}">
         <h3>Вопрос {{$k + 1}}</h3>
         {!! $question->question !!}
         @foreach ($answers[$question->id] as $answer)
             @if ($answer->correct)
-                <div class="correct-answer"><i class="fa fa-check"></i>{!! $answer->answer !!}</div>
+                <div class="answer correct"><i class="fa fa-check"></i>{!! $answer->answer !!}</div>
             @elseif (isset($answerChecked[$answer->id]))
-                <div class="incorrect-answer"><span class="empty"></span>{!! $answer->answer !!}</div>
+                <div class="answer incorrect"><span class="empty"></span>{!! $answer->answer !!}</div>
             @else
-                <div><span class="empty"></span>{!! $answer->answer !!}</div>
+                <div class="answer"><span class="empty"></span>{!! $answer->answer !!}</div>
             @endif
         @endforeach
-        <br>
     </div>
     @endif
 @endforeach
@@ -33,17 +32,18 @@
 <form method="post">
 {{ csrf_field() }}
 @foreach ($questions as $k => $question)
-    <div class="row">
+    <div class="question">
         <h3>Вопрос {{$k + 1}}</h3>
         {!! $question->question !!}
         @foreach ($answers[$question->id] as $answer)
+        <div class="answer">
             <input type="radio" name="answers[{{ $question->id }}]" id="answer_{{ $answer->id }}" value="{{ $answer->id }}"{{ isset($answerChecked[$answer->id]) ? ' checked' : '' }}>
-            <label for="answer_{{ $answer->id }}">{!! $answer->answer !!}</label><br>
+            <label for="answer_{{ $answer->id }}">{!! $answer->answer !!}</label>
+        </div>
         @endforeach
-        <br>
     </div>
 @endforeach
-    <div class="row submit">
+    <div>
         <input type="submit" value="Завершить" class="btn">
     </div>
 </form>
